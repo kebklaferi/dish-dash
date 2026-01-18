31e92fcc0b95d1e5affd861860a2cf6f186d8cef
LopataInKramp <kskok1144@gmail.com>
2026-01-18 20:22:24 +0100

    update

--- CatalogService/obj/Debug/net9.0/CatalogService.AssemblyInfo.cs
+++ CatalogService/obj/Debug/net9.0/CatalogService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192682affd88451fc5300f24552498eb81b7e868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788d3b8ee334ba9c76919f78effc7b4205cf1782")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
